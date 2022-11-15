a05912d97649717401c7d1f2e884f6a13819ed7b
bhukya <janardhanbhukya002@gmail.com>
2022-11-15 19:29:27 +0530

    MAVEN

---
+++ src/test/java/mavan/LoginTest.java
@@ -0,0 +1,10 @@
+package mavan;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+@Test
+public void loginTest() {
+	System.out.println("login done");
+}
+}
---
+++ src/test/java/mavan/RegTest.java
@@ -0,0 +1,12 @@
+package mavan;
+
+import org.testng.annotations.Test;
+
+public class RegTest {
+	@Test
+	public void regTest() {
+
+
+		System.out.print("reg done");
+	}
+}
